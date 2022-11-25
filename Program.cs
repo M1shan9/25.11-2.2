25c10fd93e0aab15faeca13fbfb184570ae77025
M1shan9 <118421201+M1shan9@users.noreply.github.com>
2022-11-25 16:52:57 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._11_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   Console.WriteLine("Введите любое число: ");
+   int n = Convert.ToInt32(Console.ReadLine());
+   if (n > 48)
+    Console.WriteLine("Ответ: " + (n - 123) * 3);
+   else
+    Console.WriteLine("Ответ: " + Math.Abs(n - 123));
+  }
+    }
+}
